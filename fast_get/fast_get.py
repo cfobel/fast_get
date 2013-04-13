b72a4c404d3bd18e629e7a2ce44753ea0fbd7202
Christian Fobel <christian@fobel.net>
2013-04-13 13:52:33 -0400

    Commit of project skeleton for parallel downloader

---
+++ fast_get/fast_get.py
@@ -0,0 +1,8 @@
+def fast_get(url, connections=1):
+    # Determine the size of the file at the URL
+
+    # Split the file into as many sections as we have connections
+
+    # Start a separate read co-routine for each section
+
+    # Wait for all sections to finish, or error
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = 'fast_get',
+    version = '0.0.1',
+    description = 'Multiple connection download of specified URL',
+    keywords = 'download fast accelerator parallel',
+    author = 'Christian Fobel',
+    url = 'https://github.com/cfobel/fast_get',
+    license = 'GPL',
+    long_description = '''''',
+    packages = ['fast_get'],
+    #scripts = ['scripts/fast_get']
+)
