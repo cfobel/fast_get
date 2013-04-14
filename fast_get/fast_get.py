dadfc694cb6334aee884a0fc10e0921ee539d4cd
Christian Fobel <christian@fobel.net>
2013-04-14 03:27:42 -0400

    [WIP] Fix so parallel data matches, use threads...
    Use threads instead of greenlets since download rates did not seem to be
    very high using greenlets.  This might be due to me not using the
    `requests` package properly with `gevent`.  Perhaps it is worthwhile to
    try again using monkey-patched `urllib2`.

--- fast_get/fast_get.py
+++ fast_get/fast_get.py
@@ -2,15 +2,31 @@ from __future__ import division
 import re
 from collections import OrderedDict
 import cStringIO as StringIO
+from copy import deepcopy
+from datetime import datetime
+from threading import Thread
+import time
 
 import numpy as np
 import socket
 import requests
-import gevent
-import zmq.green as gzmq
+import zmq
 
 
 socket.setdefaulttimeout(0.0001)
+
+
+def remove_overlaps(input_data):
+    data = deepcopy(input_data)
+    to_remove = set()
+    for i in range(len(data) - 1):
+        for k in range(i + 1, len(data)):
+            if data[i][1] >= data[k][0]:
+                data[i][1] = data[k][1]
+                to_remove.add(k)
+            else:
+                break
+    return [d for i, d in enumerate(data) if i not in to_remove]
 
 
 def get_range_responses(url, connection_count=4, byte_count=None):
@@ -39,7 +55,7 @@ def get_range_responses(url, connection_count=4, byte_count=None):
     # one call by accessing the `content` attribute.
     section_byte_ranges = [(sum(section_byte_counts[:i]), sum(section_byte_counts[:i + 1]) - 1) for i in range(len(section_byte_counts))]
     data = [requests.get(url, stream=True, headers={'Range': 'bytes=%s-%s' % r}) for r in section_byte_ranges]
-    return data
+    return byte_count, data
 
 
 cre_byte_range = re.compile(r'bytes (?P<start>\d+)\s*-\s*(?P<end>\d+)\s*/\s*(?P<total>\d+)')
@@ -50,10 +66,13 @@ def get_range_info(range_str):
     return OrderedDict([(k, int(data[k])) for k in ('start', 'end', 'total')])
 
 
-def get_data(i, stream_response, push):
+def get_data(i, stream_response, chunk_size=(1 << 10)):
+    ctx = zmq.Context.instance()
+    push = zmq.Socket(ctx, zmq.PUSH)
+    push.connect('inproc://queue')
     byte_count = int(stream_response.headers['content-length'])
     range_info = get_range_info(stream_response.headers['content-range'])
-    content_iterator = stream_response.iter_content(chunk_size=(8 << 10))
+    content_iterator = stream_response.iter_content(chunk_size=chunk_size)
     bytes_seen = 0
     failure_count = 0
 
@@ -71,58 +90,69 @@ def get_data(i, stream_response, push):
         except Exception, e:
             import traceback; traceback.print_exc()
             failure_count += 1
-        gevent.sleep()
     print dict(bytes_seen=bytes_seen, failure_count=failure_count)
+    push.close()
+    del push
+    del ctx
 
 
-def fast_get(url, connection_count=4, byte_count=None):
-    ctx = gzmq.Context.instance()
-    push = gzmq.Socket(ctx, gzmq.PUSH)
-    pull = gzmq.Socket(ctx, gzmq.PULL)
-    push.bind('inproc://queue')
-    pull.connect('inproc://queue')
+def get_ranges(data, value):
+    return [i for i, d in enumerate(data) if value >= d[0] and value <= d[1]]
 
-    responses = get_range_responses(url, 4, byte_count=byte_count)
-    tasklets = [gevent.spawn(get_data, i, r, push) for i, r in enumerate(responses)]
 
-    tasklet_count = len(tasklets)
-    joined = set()
+def fast_get(url, connection_count=4, byte_count=None, chunk_size=(1 << 10)):
+    ctx = zmq.Context.instance()
+    pull = zmq.Socket(ctx, zmq.PULL)
+    pull.bind('inproc://queue')
+
+    byte_count, responses = get_range_responses(url, connection_count,
+                                                byte_count=byte_count)
+    threads = [Thread(target=get_data, args=(i, r, chunk_size))
+               for i, r in enumerate(responses)]
+    for t in threads:
+        t.start()
+
     output = StringIO.StringIO()
-    while len(joined) < tasklet_count:
-        for t in tasklets:
-            if t.ready() and not t in joined:
-                t.join()
-                joined.add(t)
-        gevent.sleep(0.001)
-    print 'joined greenlets'
+    ranges = []
+
     while True:
         try:
-            start, end, data = pull.recv_multipart(flags=gzmq.NOBLOCK)
+            start, end, data = pull.recv_multipart(flags=zmq.NOBLOCK)
             output.seek(int(start))
+            ranges.append(map(int, (start, end)))
+            ranges = remove_overlaps(sorted(ranges))
             output.write(data)
-            if output.tell() >= byte_count:
+            if len(ranges) == 1 and ranges[0][0] == 0 and ranges[0][1] == byte_count:
                 break
-        except gzmq.ZMQError, e:
-            if e.errno == gzmq.EAGAIN:
-                break
-            raise
-        gevent.sleep(0.01)
+        except zmq.ZMQError, e:
+            if e.errno == zmq.EAGAIN:
+                pass
+            else:
+                raise
+        time.sleep(0.001)
     print 'wrote sections'
     result = output.getvalue()
     output.close()
-    push.close()
-    del push
     del ctx
     return result
 
 
 def main():
-    byte_count = 305430
+    byte_count = 10 << 20
     url = 'http://remote.fobel.net/pub/0cadd539238263aaca0915b3bd065ca7/The.Office.US.S09E19.HDTV.x264-LOL.mp4'
-    output = fast_get(url, 4, byte_count)
+
+    for chunk_size in [12 << 10]:
+        start = datetime.now()
+        output = fast_get(url, 5, byte_count, chunk_size)
+        end = datetime.now()
+        print 'chunk_size:', chunk_size, (byte_count >> 10) / (end - start).total_seconds()
+
+    start = datetime.now()
     verify_data = requests.get(url, stream=True, headers={'Range': 'bytes=0-%s'
                                                           % (byte_count -
                                                              1)}).content
+    end = datetime.now()
+    print (byte_count >> 10) / (end - start).total_seconds()
     assert(len(output) == len(verify_data))
     if not (output == verify_data):
         output_array = np.array([v for v in output])
